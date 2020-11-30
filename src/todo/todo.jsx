7010c538cecbc01af8660b3ef377f1d3b58e391b
Priscila Silva <priscila.rodrigues@hotmail.com.br>
2020-11-30 20:21:11 +0100

    first commit

---
+++ src/todo/todo.jsx
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import PageHeader from "../template/pageHeader";
+import TodoForm from "./todoForm";
+import TodoList from "./todoList";
+
+const URL = "http://localhost:3004/api/todos";
+
+export default (props) => {
+  const [description, setDescription] = useState("");
+  const [list, setList] = useState([]);
+
+  useEffect(() => refresh(), []);
+
+  const refresh = (desc = "") => {
+    const search = desc ? `&description__regex=/${desc}/` : "";
+    console.log(`${URL}?sort=-createdAt${search}`);
+    axios.get(`${URL}?sort=-createdAt${search}`).then((resp) => {
+      setList(resp.data);
+      setDescription(desc);
+    });
+  };
+
+  const handleSearch = () => {
+    refresh(description);
+  };
+
+  const handleRemove = (todo) => {
+    axios.delete(`${URL}/${todo._id}`).then((resp) => {
+      refresh(description);
+    });
+  };
+
+  const handleChange = (e) => {
+    setDescription(e.target.value);
+  };
+
+  const handleMarkAsDone = (todo) => {
+    axios
+      .put(`${URL}/${todo._id}`, { done: true })
+      .then((resp) => refresh(description));
+  };
+
+  const handleMarkAsPending = (todo) => {
+    axios
+      .put(`${URL}/${todo._id}`, { done: false })
+      .then((resp) => refresh(description));
+  };
+
+  const handleAdd = () => {
+    axios.post(URL, { description }).then((resp) => refresh());
+  };
+
+  const handleClear = () => {
+    refresh();
+  };
+
+  return (
+    <div>
+      <PageHeader name="Tarefas" small="Cadastro" />
+      <TodoForm
+        description={description}
+        handleChange={handleChange}
+        handleAdd={handleAdd}
+        handleSearch={handleSearch}
+        handleClear={handleClear}
+      />
+      <TodoList
+        list={list}
+        handleRemove={handleRemove}
+        handleMarkAsPending={handleMarkAsPending}
+        handleMarkAsDone={handleMarkAsDone}
+      />
+    </div>
+  );
+};
