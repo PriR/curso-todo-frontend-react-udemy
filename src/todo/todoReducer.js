d39cfc459490364cc12915033644b1b81f71e246
Priscila Silva <priscila.rodrigues@hotmail.com.br>
2020-12-04 17:01:49 +0100

    Fix name type reducer

--- src/todo/todoReducer.js
+++ src/todo/todoReducer.js
@@ -21,7 +21,7 @@ const INITIAL_STATE = {
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case "DESCRIPTION_CHANGE":
+    case "DESCRIPTION_CHANGED":
       return { ...state, description: action.payload };
     default:
       return state;
