3ac19bb48178f23287414800346b89069fbe2220
Priscila Silva <priscila.rodrigues@hotmail.com.br>
2020-12-04 02:07:16 +0100

    removing unecessary import

--- src/index.jsx
+++ src/index.jsx
@@ -5,8 +5,6 @@ import { Provider } from "react-redux";
 import App from "./main/app";
 import reducers from "./main/reducers";
 
-// import { composeWithDevTools } from "redux-devtools-extension";
-
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
 const store = createStore(
